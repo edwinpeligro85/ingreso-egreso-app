8b3c73fb852be3afa2cc01a61f98d3c11839c6fc
Edwin Serna <desarrolladorjr5.spv@gmail.com>
2020-10-31 23:42:52 -0500

    Template listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '', component: DashboardComponent, children: [
+      { path: '', component: EstadisticaComponent },
+      { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+      { path: 'detalle', component: DetalleComponent }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
